f8f869b16528613056a8ab4d5d7f4afc68d5062c
zureealLV <1073166164@qq.com>
2026-05-19 00:07:47 +0800

    Twikoo 换成自己的后端

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -6,7 +6,7 @@ export const commentConfig: CommentConfig = {
 
 	//twikoo评论系统配置，版本1.7.4
 	twikoo: {
-		envId: "https://twikoo.vercel.app",
+		envId: "https://zureeal.vercel.app",
 		// 设置 Twikoo 评论系统语言
 		lang: "zh-CN",
 		// 是否启用文章访问量统计功能
